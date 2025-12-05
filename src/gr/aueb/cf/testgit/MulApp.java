6bcb6445c3604a3131dc2811193785cfa95732ea
Theodora Antonakopoulou <theoantonakop@gmail.com>
2025-12-05 23:06:17 +0200

    feat: add mul-app

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+    public static void main(String[] args) {
+
+    }
+    public static int multiplay(int a, int b){
+        return a*b;
+
+    }
+}
