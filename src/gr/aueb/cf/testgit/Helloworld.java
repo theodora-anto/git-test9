9a088a24b2c4cb3adacea11f32ba919dbf6a5236
Theodora Antonakopoulou <theoantonakop@gmail.com>
2025-12-06 00:48:50 +0200

    add: hello changes

--- src/gr/aueb/cf/testgit/Helloworld.java
+++ src/gr/aueb/cf/testgit/Helloworld.java
@@ -3,7 +3,6 @@ package gr.aueb.cf.testgit;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello coding festival!!!!");
-        System.out.println("Hello coding factory!!!!!");
+
     }
 }
