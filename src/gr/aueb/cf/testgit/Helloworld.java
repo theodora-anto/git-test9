597d13fafb51ef106655a07d11540176903b6582
Theodora Antonakopoulou <theoantonakop@gmail.com>
2025-12-05 16:49:39 +0200

    fix: add print hello coding

--- src/gr/aueb/cf/testgit/Helloworld.java
+++ src/gr/aueb/cf/testgit/Helloworld.java
@@ -3,5 +3,6 @@ package gr.aueb.cf.testgit;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello coding");
     }
 }
